9c46eb09e6abe5fadbdf618c257bd7f454060114
cmstucke <cmstucke@gmail.com>
2024-01-06 20:11:56 -0500

    recipe model and sample migration gitignore

---
+++ app/models/recipe.py
@@ -0,0 +1,30 @@
+from .db import db, environment, SCHEMA, add_prefix_for_prod
+
+class Recipe(db.Model):
+  __tablename__ = 'recipes'
+
+  if environment == 'production':
+  __table_args__ = { 'schema': SCHEMA }
+
+  id = db.Column(db.Integer, primary_key=True)
+  owner_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('users.id')), nullable=False)
+  title = db.Column(db.String, nullable=False)
+  category = db.Column(db.String, nullable=False)
+  description = db.Column(db.String, nullable=False)
+  ingredients = db.Column(db.String, nullable=False)
+  steps = db.Column(db.String, nullable=False)
+  background = db.Column(db.String, nullable=False)
+
+  users = db.relationship('User', back_populates='recipes')
+
+  def to_dict(self):
+    return {
+      'id': self.id,
+      'owner_id': self.owner_id,
+      'title': self.title,
+      'category': self.category,
+      'description': self.description,
+      'ingredients': self.ingredients,
+      'steps': self.steps,
+      'background': self.background
+    }
